bd8dcd21e7ffcaad48667140858de548d450bec9
Krish lad <kcl@narola.email>
2026-04-07 16:55:40 +0530

    Change the access key

--- src/components/ContactUS/Contact.jsx
+++ src/components/ContactUS/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    formData.append("access_key", import.meta.env.VITE_EMAIL_ACCESSKEY);
+    formData.append("access_key", import.meta.env.VITE_EMAIL_ACCESSKEY );
 
     const res = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
