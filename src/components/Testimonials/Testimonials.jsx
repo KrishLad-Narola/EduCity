0d39cda5e4b0b22aa0d35d8072ac8dfd3efe1e09
Krish lad <kcl@narola.email>
2026-04-08 09:11:47 +0530

    Updated the next button and back button in testimonials.jsx file with changes in IF Condition. Now Testimonials part is wroking flow.

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,36 +1,47 @@
 import React, { useRef } from "react";
-import "./Testimonials.css"
-import next_icon from "../../assets/next-icon.png"
-import back_icon from "../../assets/back-icon.png"
-import User_1 from "../../assets/user-1.png"
-import User_2 from "../../assets/user-2.png"
-import User_3 from "../../assets/user-3.png"
-import User_4 from "../../assets/user-4.png"
+import "./Testimonials.css";
 
+import next_icon from "../../assets/next-icon.png";
+import back_icon from "../../assets/back-icon.png";
+import User_1 from "../../assets/user-1.png";
+import User_2 from "../../assets/user-2.png";
+import User_3 from "../../assets/user-3.png";
+import User_4 from "../../assets/user-4.png";
 
 const Testimonials = () => {
   
-   const slider =  useRef();
-   let tx = 0;
+  const slider = useRef(null);
+  const tx = useRef(0); 
 
   const SlideForward = () => {
-      if(tx > 50){
-        tx -= 25;
-      }
-      slider.current.style.transform = `translateX(${tx}%)`;
-  }
+    if (tx.current > -50) {   
+      tx.current -= 25;
+    }
+    slider.current.style.transform = `translateX(${tx.current}%)`;
+  };
 
   const SlideBackward = () => {
-      if(tx < 0){
-        tx += 25;
-      }
-      slider.current.style.transform = `translateX(${tx}%)`;
-  }
+    if (tx.current < 0) {
+      tx.current += 25;
+    }
+    slider.current.style.transform = `translateX(${tx.current}%)`;
+  };
 
   return (
     <div className="testimonials">
-      <img src={next_icon} alt="" className="next-btn" onClick={SlideForward} />
-      <img src={back_icon} alt="" className="back-btn" onClick={SlideBackward}/>
+      <img
+        src={next_icon}
+        alt="Next"
+        className="next-btn"
+        onClick={SlideForward}
+      />
+      <img
+        src={back_icon}
+        alt="Back"
+        className="back-btn"
+        onClick={SlideBackward}
+      />
+
       <div className="slider">
         <ul ref={slider}>
           <li>
@@ -41,9 +52,12 @@ const Testimonials = () => {
               <div>
                 <h3>Krishi Mistry</h3>
                 <span>Educity , India</span>
-                <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made.
-                  The supportive community, state-of-the-art facilities,
-                  and commitment to academic excellence have truly exceeded my expectations.</p>
+                <p>
+                  Choosing to pursue my degree at Edusity was one of the best
+                  decisions I've ever made. The supportive community,
+                  state-of-the-art facilities, and commitment to academic
+                  excellence have truly exceeded my expectations.
+                </p>
               </div>
             </div>
           </li>
@@ -56,9 +70,12 @@ const Testimonials = () => {
               <div>
                 <h3>Krish Lad</h3>
                 <span>Educity , India</span>
-                <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made.
-                  The supportive community, state-of-the-art facilities,
-                  and commitment to academic excellence have truly exceeded my expectations.</p>
+                <p>
+                  Choosing to pursue my degree at Edusity was one of the best
+                  decisions I've ever made. The supportive community,
+                  state-of-the-art facilities, and commitment to academic
+                  excellence have truly exceeded my expectations.
+                </p>
               </div>
             </div>
           </li>
@@ -71,9 +88,12 @@ const Testimonials = () => {
               <div>
                 <h3>Heer Prajapti</h3>
                 <span>Educity , India</span>
-                <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made.
-                  The supportive community, state-of-the-art facilities,
-                  and commitment to academic excellence have truly exceeded my expectations.</p>
+                <p>
+                  Choosing to pursue my degree at Edusity was one of the best
+                  decisions I've ever made. The supportive community,
+                  state-of-the-art facilities, and commitment to academic
+                  excellence have truly exceeded my expectations.
+                </p>
               </div>
             </div>
           </li>
@@ -86,13 +106,15 @@ const Testimonials = () => {
               <div>
                 <h3>Smit Mistry</h3>
                 <span>Educity , India</span>
-                <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made.
-                  The supportive community, state-of-the-art facilities,
-                  and commitment to academic excellence have truly exceeded my expectations.</p>
+                <p>
+                  Choosing to pursue my degree at Edusity was one of the best
+                  decisions I've ever made. The supportive community,
+                  state-of-the-art facilities, and commitment to academic
+                  excellence have truly exceeded my expectations.
+                </p>
               </div>
             </div>
           </li>
-
         </ul>
       </div>
     </div>
